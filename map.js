f77d8f052c1c54b81038019027e31609bde6dff2
Heather Conley <hconley6695@gmail.com>
2017-09-26 22:09:21 -0400

    reorganized project and tried to pull marker onto map

--- map.js
+++ map.js
@@ -5,4 +5,15 @@ function initMap() {
 		center: {lat: 33.7490, lng: -84.3880},
 		zoom: 12
 	});
+
+	var piedmont = {lat: 33.7901,lng: -84.3696 };
+
+	var marker = new google.maps.Marker({
+		position: piedmont,
+		map: map,
+		title: 'Piedmont Park',
+		icon: './images/marker-image.png'
+	});
+
+	
 }
