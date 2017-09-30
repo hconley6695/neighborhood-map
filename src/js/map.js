d1ef6eb4c218c1564bc13fb25b226014c490efcc
Heather Conley <hconley6695@gmail.com>
2017-09-30 10:31:57 -0400

    cleaned up files, put all in source folder and deleted the others, added a good marker and infowindow to the single marker

--- src/js/map.js
+++ src/js/map.js
@@ -5,4 +5,24 @@ function initMap() {
 		center: {lat: 33.7490, lng: -84.3880},
 		zoom: 12
 	});
+
+	var piedmont = {lat: 33.7901,lng: -84.3696 };
+
+	var marker = new google.maps.Marker({
+		position: piedmont,
+		map: map,
+		title: 'Piedmont Park',
+		icon: './images/rsz_1marker-image.png'
+	});
+
+	var infoWindow = new google.maps.InfoWindow({
+		content: 'Hello, this is Piedmont Park.'
+
+	});
+
+	marker.addListener('click', function() {
+		infoWindow.open(map, marker);
+	});
+
+	
 }
