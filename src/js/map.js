a5307a1d73fc9d16a504f2132e686fa01e3f5f06
Heather Conley <hconley6695@gmail.com>
2017-09-30 11:34:59 -0400

    placed viewmodel information into same js file as functions; multiple markers showing on page, and each has an infowindow popping up with place's name

--- src/js/map.js
+++ src/js/map.js
@@ -1,4 +1,35 @@
+// import places from  './viewmodel';
+
 var map;
+
+// create new empty array for all places
+var markers = [];
+
+var places = [
+	{
+		name: 'Piedmont Park',
+		position: {lat: 33.7901,lng: -84.3696 }
+	},
+
+	{
+		name: 'Georgia Aquarium',
+		position: {lat: 33.7634,lng: -84.3951}
+	},
+	{
+		name: 'Ponce City Market',
+		position: {lat: 33.7719,lng: -84.3665}
+	},
+	{
+		name: 'Zoo Atlanta',
+		position: {lat: 33.7341,lng: -84.3723}
+	},
+	{
+		name: 'Oakland Cemetary',
+		position: {lat: 33.7486,lng: -84.3730}
+	},
+
+]
+
 
 function initMap() {
 	map = new google.maps.Map(document.getElementById('map'), {
@@ -6,23 +37,66 @@ function initMap() {
 		zoom: 12
 	});
 
-	var piedmont = {lat: 33.7901,lng: -84.3696 };
+	var largeInfowindow = new google.maps.InfoWindow();
 
-	var marker = new google.maps.Marker({
-		position: piedmont,
-		map: map,
-		title: 'Piedmont Park',
-		icon: './images/rsz_1marker-image.png'
-	});
+	for (var i = 0; i < places.length; i++) {
+		// get the positions of the array of locations from viewmodel.js
+		var position = places[i].position;
+		// get the name of the array of locations from viewmodel.js
+		var name = places[i].name;
+		// creating a marker for each location
+		var marker = new google.maps.Marker({
+			position: position,
+			map: map,
+			title: name,
+			animation: google.maps.Animation.DROP,
+			icon: './images/rsz_1marker-image.png',
+			id: i
+		});
+		// putting all of the markers into the new markers array
+		markers.push(marker);
+		// when you click each marker, it will show an info window
+		marker.addListener('click', function() {
+			contentInfoWindow(this, largeInfowindow);
+		});
 
-	var infoWindow = new google.maps.InfoWindow({
-		content: 'Hello, this is Piedmont Park.'
+	}
 
-	});
+	function contentInfoWindow(marker, infowindow) {
 
-	marker.addListener('click', function() {
-		infoWindow.open(map, marker);
-	});
+		if (infowindow.marker != marker) {
+			infowindow.marker = marker;
+			infowindow.setContent('<span>'+ marker.title +'</span>');
+
+			infowindow.open(map, marker);
+
+			infowindow.addListener('closeclick', function() {
+				// infowindow.setMarker(null);
+			})
+
+		}
+	}
+
+	// var piedmont = {
+	// 	position: {lat: 33.7901,lng: -84.3696 },
+	// 	name: 'Piedmont Park'
+	// };
+
+	// var marker = new google.maps.Marker({
+	// 	position: piedmont.position,
+	// 	map: map,
+	// 	title: 'Piedmont Park',
+	// 	icon: './images/rsz_1marker-image.png'
+	// });
+
+	// var infoWindow = new google.maps.InfoWindow({
+	// 	content: '<span data-bind="text: name">This is my span</span>'
+
+	// });
+
+	// marker.addListener('click', function() {
+	// 	infoWindow.open(map, marker);
+	// });
 
 	
 }
