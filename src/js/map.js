d7f141c7b83ebc131eec6110c980e074bee48a16
Heather Conley <hconley6695@gmail.com>
2017-10-02 22:32:12 -0400

    reorganized code to include viewModel function so that markers were populating by this and self

--- src/js/map.js
+++ src/js/map.js
@@ -1,15 +1,9 @@
-// import places from  './viewmodel';
-
-var map;
-
-// create new empty array for all places
-var markers = [];
-
+// model
 var places = [
 	{
 		name: 'Piedmont Park',
 		position: {lat: 33.7901,lng: -84.3696 },
-		type: 'parks'
+		type: 'parks',
 	},
 
 	{
@@ -58,143 +52,175 @@ var places = [
 		position: {lat: 33.844844,lng: -84.427154},
 		type: 'restaurants'
 	},
+];
+
+// creating global map variable
+var map;
 
 
-]
+function viewModel() {
+	var self = this;
 
+	this.search = ko.observable('');
 
-function initMap() {
-	map = new google.maps.Map(document.getElementById('map'), {
-		center: {lat: 33.7490, lng: -84.3880},
-		zoom: 12
-	});
+	// create new empty array for all places
+	this.markers = [];
 
-	var largeInfowindow = new google.maps.InfoWindow();
-
-	for (var i = 0; i < places.length; i++) {
-		// get the positions of the array of locations from viewmodel.js
-		var position = places[i].position;
-		// get the name of the array of locations from viewmodel.js
-		var name = places[i].name;
-
-		var type = places[i].type;
-		// creating a marker for each location
-			// map: map,		
-			var marker = new google.maps.Marker({
-			position: position,
-			title: name,
-			type: type,
-			animation: google.maps.Animation.DROP,
-			icon: './images/rsz_1marker-image.png',
-			id: i
-		});
-		// putting all of the markers into the new markers array
-		markers.push(marker);
-		// when you click each marker, it will show an info window
-		marker.addListener('click', function() {
-			contentInfoWindow(this, largeInfowindow);
-		});
-
-	}
-
-	function contentInfoWindow(marker, infowindow) {
+	this.contentInfoWindow = function(marker, infowindow) {
 
 		if (infowindow.marker != marker) {
 			infowindow.marker = marker;
-			infowindow.setContent('<span>'+ marker.title +'</span>');
+			infowindow.setContent('<span>'+ this.marker.title +'</span>');
 
 			infowindow.open(map, marker);
 
 			infowindow.addListener('closeclick', function() {
-				// infowindow.setMarker(null);
-			})
+				infowindow.marker = null;
+			});
 
 		}
-	}
+	};
 
-	document.getElementById('no-filter').addEventListener('click', showAll);
-	document.getElementById('attractions-filter').addEventListener('click', showAttractions);
-	document.getElementById('parks-filter').addEventListener('click', showParks);
-	document.getElementById('restaurants-filter').addEventListener('click', showRestaurants);
-	document.getElementById('shopping-filter').addEventListener('click', showShopping);
+
+	this.initMap = function() {
+		map = new google.maps.Map(document.getElementById('map'), {
+			center: {lat: 33.7490, lng: -84.3880},
+			zoom: 12
+		});
+
+
+		this.largeInfowindow = new google.maps.InfoWindow();
+
+	//Places markers on the map within the initMap function
+    //Puts the infoWindows on each individual marker
+		for (var i = 0; i < places.length; i++) {
+			// get the positions of the array of locations from viewmodel.js
+			this.position = places[i].position;
+			// get the name of the array of locations from viewmodel.js
+			this.name = places[i].name;
+
+			this.type = places[i].type;
+			// creating a marker for each location	
+			this.marker = new google.maps.Marker({
+				map: map,
+				position: this.position,
+				title: this.name,
+				type: this.type,
+				animation: google.maps.Animation.DROP,
+				icon: './images/rsz_1marker-image.png',
+				id: i
+			});
+
+			this.marker.setMap(map);
+			// putting all of the markers into the new markers array
+			this.markers.push(this.marker);
+			// when you click each marker, it will show an info window
+			this.marker.addListener('click', function() {
+				self.contentInfoWindow(this, self.largeInfowindow);
+			});
+		}			
+	};
+
+	this.initMap();
+
+}
+
+
+
+// CHANGING CODE HERE
+
+
+
+
 	
-	function showAll() {
+// function showAll() {
 
-		for (var i = 0; i < markers.length; i++) {
-			markers[i].setMap(null);
-			markers[i].setMap(map);
-		}
-	}
+// 	for (var i = 0; i < markers.length; i++) {
+// 		markers[i].setMap(null);
+// 		markers[i].setMap(map);
+// 	}
+// }
 
-	function showAttractions() {
-		console.log(markers);
-		for (var i = 0; i < markers.length; i++) {
-			markers[i].setMap(null);
-			if (markers[i].type == 'attractions') {
+	// document.getElementById('no-filter').addEventListener('click', showAll);
+	// document.getElementById('attractions-filter').addEventListener('click', showAttractions);
+	// document.getElementById('parks-filter').addEventListener('click', showParks);
+	// document.getElementById('restaurants-filter').addEventListener('click', showRestaurants);
+	// document.getElementById('shopping-filter').addEventListener('click', showShopping);
+// var viewModel = {
+	// query: ko.observable('')
+// }
+	
+// TO CHANGE KO.OBSERVABLE, YOU PLACE A NEW VALUE INTO IT-- EX: VAR NUM = KO.OBSERVABLE(42);
+// KNOCKOUT MAKES YOU RUN A FUNCITON IN ORDER TO CHANGE THE VALUE  -- EX. NUM(43);
 
-				markers[i].setMap(map);
-			} else {
-				document.getElementById('no-possibilities').innerHTML = '<span> There are no results.</span>';
-			}
-		}
-	}
+// viewModel.markers = function() {
 
-	function showParks() {
-		for (var i = 0; i < markers.length; i++) {
-			markers[i].setMap(null);
-			if (markers[i].type == 'parks') {
+// }
 
-				markers[i].setMap(map);
-			} else {
-				document.getElementById('no-possibilities').innerHTML = '<span> There are no results.</span>';
-			}
-		}
-	}
 
-	function showRestaurants() {
-		for (var i = 0; i < markers.length; i++) {
-			markers[i].setMap(null);
-			if (markers[i].type == 'restaurants') {
+// ko.applyBindings(viewModel);
 
-				markers[i].setMap(map);
-			} else {
-				document.getElementById('no-possibilities').innerHTML = '<span> There are no results.</span>';
-			}
-		}
-	}
+	// function showAttractions() {
+	// 	console.log(markers);
+	// 	for (var i = 0; i < markers.length; i++) {
+	// 		markers[i].setMap(null);
+	// 		if (markers[i].type == 'attractions') {
 
-	function showShopping() {
+	// 			markers[i].setMap(map);
+	// 		} else {
+	// 			document.getElementById('no-possibilities').innerHTML = '<span> There are no results.</span>';
+	// 		}
+	// 	}
+	// }
 
-		for (var i = 0; i < markers.length; i++) {
-			markers[i].setMap(null);
-			if (markers[i].type == 'shopping') {
+	// function showParks() {
+	// 	for (var i = 0; i < markers.length; i++) {
+	// 		markers[i].setMap(null);
+	// 		if (markers[i].type == 'parks') {
+
+	// 			markers[i].setMap(map);
+	// 		} else {
+	// 			document.getElementById('no-possibilities').innerHTML = '<span> There are no results.</span>';
+	// 		}
+	// 	}
+	// }
+
+	// function showRestaurants() {
+	// 	for (var i = 0; i < markers.length; i++) {
+	// 		markers[i].setMap(null);
+	// 		if (markers[i].type == 'restaurants') {
+
+	// 			markers[i].setMap(map);
+	// 		} else {
+	// 			document.getElementById('no-possibilities').innerHTML = '<span> There are no results.</span>';
+	// 		}
+	// 	}
+	// }
+
+	// function showShopping() {
+
+	// 	for (var i = 0; i < markers.length; i++) {
+	// 		markers[i].setMap(null);
+	// 		if (markers[i].type == 'shopping') {
 		
-				markers[i].setMap(map);
-			} else {
-				document.getElementById('no-possibilities').innerHTML = '<span> There are no results.</span>';
-			}
-		}
-	}
-	// var piedmont = {
-	// 	position: {lat: 33.7901,lng: -84.3696 },
-	// 	name: 'Piedmont Park'
-	// };
+	// 			markers[i].setMap(map);
+	// 		} else {
+	// 			document.getElementById('no-possibilities').innerHTML = '<span> There are no results.</span>';
+	// 		}
+	// 	}
+	// }
 
-	// var marker = new google.maps.Marker({
-	// 	position: piedmont.position,
-	// 	map: map,
-	// 	title: 'Piedmont Park',
-	// 	icon: './images/rsz_1marker-image.png'
-	// });
+// }	
+// }
 
-	// var infoWindow = new google.maps.InfoWindow({
-	// 	content: '<span data-bind="text: name">This is my span</span>'
 
-	// });
+// var myViewModel = {
+// 	name: ko.observable('Bob'),
+// 	age: ko.observable(23)
+// };
 
-	// marker.addListener('click', function() {
-	// 	infoWindow.open(map, marker);
-	// });
-
-	
+// ko.applyBindings(myViewModel); 
+// ko.applyBindings(viewmodel);
+function startApp() {
+	ko.applyBindings(new viewModel());
 }
